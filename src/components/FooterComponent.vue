c4de7e5a46b893c3c2ab77ed36d84850208d932e
Metastem <110768529+Metastem@users.noreply.github.com>
2022-12-02 18:44:47 +0800

    Update FooterComponent.vue

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,24 +1,7 @@
 <template>
     <footer class="text-center py-4 rounded-xl children:(mx-3) w-full mt-10 mb-5">
-        <a aria-label="GitHub" href="https://github.com/TeamPiped/Piped" target="_blank">
-            <font-awesome-icon :icon="['fab', 'github']" />
+        <a aria-label="Search" href="https://metastem.su/" target="_blank">
             <span class="ml-2" v-t="'actions.source_code'" />
-        </a>
-        <a href="https://docs.piped.video/" target="_blank">
-            <font-awesome-icon :icon="['fa', 'book']" />
-            <span class="ml-2" v-t="'actions.documentation'" />
-        </a>
-        <a href="https://github.com/TeamPiped/Piped#donations" target="_blank">
-            <font-awesome-icon :icon="['fab', 'bitcoin']" />
-            <span class="ml-2" v-t="'actions.donations'" />
-        </a>
-        <a v-if="statusPageHref" :href="statusPageHref">
-            <font-awesome-icon :icon="['fa', 'server']" />
-            <span class="ml-2" v-t="'actions.status_page'" />
-        </a>
-        <a v-if="donationHref" :href="donationHref">
-            <font-awesome-icon :icon="['fa', 'donate']" />
-            <span class="ml-2" v-t="'actions.instance_donations'" />
         </a>
     </footer>
 </template>
